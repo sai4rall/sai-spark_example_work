466874dffb77ed263f9128283a5d42dd1f37e59b
AK <sai4rall@gmail.com>
2022-02-07 23:33:42 -0500

    added more transformations2

--- src/main/java/com/spark/transformations/QuollApp.java
+++ src/main/java/com/spark/transformations/QuollApp.java
@@ -13,8 +13,13 @@ import org.apache.log4j.Logger;
 import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+import org.apache.spark.sql.types.IntegerType;
+import org.apache.spark.sql.types.IntegerType$;
 
 import java.util.Map;
+
+import static com.spark.transformations.config.Constants.bucketOutputPath;
 
 public class QuollApp {
     public static void main(String[] args) {
@@ -25,10 +30,10 @@ public class QuollApp {
 
 
         //TODO need to change to database
-        Dataset qdf=session
-                .read().option("header","true").csv("/home/hduser/IdeaProjects/QuollTransformations/src/test/resources/testone.csv");
-        Transformation quollTransformations=new QuollTransformations();
-        Dataset q=quollTransformations.apply(qdf);
+        Dataset qdf = session
+                .read().option("header", "true").csv("/home/hduser/IdeaProjects/QuollTransformations/src/test/resources/testone.csv");
+        Transformation quollTransformations = new QuollTransformations();
+        Dataset q = quollTransformations.apply(qdf);
 
 
         Dataset t = session.read().option("header", "true")
@@ -47,19 +52,77 @@ public class QuollApp {
         Broadcast cellFunction = session.sparkContext().broadcast(QuollMapConstants.cellFunctionbict, QuollUtils.classTag(Map.class));
         Broadcast validscnodes = session.sparkContext().broadcast(QuollMapConstants.validiscNodeDict, QuollUtils.classTag(Map.class));
         Broadcast areaCode = session.sparkContext().broadcast(QuollMapConstants.areaCodeDict, QuollUtils.classTag(Map.class));
-        q.
-        withColumn("|telstraCellAttributes|cellFunction",UserDefinedFunctions.eaiCellFunction.apply(q.col("cell_function"))).
-        withColumn("|telstraCellAttributes|hasSpecialEvent",UserDefinedFunctions.eaiBool.apply(q.col("special_event_cell"))).
-        withColumn("|telstraCellAttributes|hasSignificantSpecialEvent",UserDefinedFunctions.eaiBool.apply(q.col("special_event"))).
-        withColumn("|telstraCellAttributes|hasPriorityAssistCustomers",UserDefinedFunctions.eaiBool.apply(q.col("priority_assist"))).
-        withColumn("|telstraCellAttributes|hasHighSeasonality",UserDefinedFunctions.eaiBool.apply(q.col("high_seasonality"))).
-        withColumn("|telstraCellAttributes|hasWirelessLocalLoopCustomers",UserDefinedFunctions.eaiBool.apply(q.col("wll"))).
-        withColumn("|telstraCellAttributes|mobileSwitchingCenter",UserDefinedFunctions.eaivalidMscNode.apply(q.col("msc_node"))).
-        withColumn("|telstraCellAttributes|mobileServiceArea",q.col("msa")).
-        withColumn("|telstraCellAttributes|quollIndex",UserDefinedFunctions.eaiInt.apply(q.col("cell_index"))).
-        withColumn("|telstraCellAttributes|closedNumberArea",UserDefinedFunctions.eaiAreaCode.apply(q.col("cna")))
-//                select("*",q.col(""))
-        .show();
+        q = q.
+                withColumn("|telstraCellAttributes|cellFunction", UserDefinedFunctions.eaiCellFunction.apply(q.col("cell_function"))).
+                withColumn("|telstraCellAttributes|hasSpecialEvent", UserDefinedFunctions.eaiBool.apply(q.col("special_event_cell"))).
+                withColumn("|telstraCellAttributes|hasSignificantSpecialEvent", UserDefinedFunctions.eaiBool.apply(q.col("special_event"))).
+                withColumn("|telstraCellAttributes|hasPriorityAssistCustomers", UserDefinedFunctions.eaiBool.apply(q.col("priority_assist"))).
+                withColumn("|telstraCellAttributes|hasHighSeasonality", UserDefinedFunctions.eaiBool.apply(q.col("high_seasonality"))).
+                withColumn("|telstraCellAttributes|hasWirelessLocalLoopCustomers", UserDefinedFunctions.eaiBool.apply(q.col("wll"))).
+                withColumn("|telstraCellAttributes|mobileSwitchingCenter", UserDefinedFunctions.eaivalidMscNode.apply(q.col("msc_node"))).
+                withColumn("|telstraCellAttributes|mobileServiceArea", q.col("msa")).
+                withColumn("|telstraCellAttributes|quollIndex", UserDefinedFunctions.eaiInt.apply(q.col("cell_index"))).
+                withColumn("|telstraCellAttributes|closedNumberArea", UserDefinedFunctions.eaiAreaCode.apply(q.col("cna")))
+                .select(q.col("*"),
+                        q.col("billing_name").alias("|telstraCellAttributes|billingName"),
+                        q.col("roamer").alias("|telstracellAttributes|iroaningAgreement"),
+                        q.col("|telstraCellAttributes|cellFunction"),
+                        q.col("|telstraCellAttributes|closedNumberArea"),
+                        q.col("coverage_classification").alias("|telstracellAttributes|coverageClassification"),
+                        functions.regexp_replace(q.col("coverage_statement"), "[\\n\\r]+", " ").alias("|telstraCellAttributes|coverageStatement"),
+                        q.col("|telstracellAttributes|hasPriorityAssistcustomers"),
+                        q.col("|telstracellAttributes|haswirelessLocalLoopCustomers"),
+                        q.col("optimisation_cluster").alias("|telstraceilAttributes|optimisationCluster"),
+                        q.col("sac_dec").alias("|telstraceilAttributes|serviceAreacode").cast(IntegerType$.MODULE$),
+                        q.col("owner").alias("|telstraceilAttributes|wirelessServiceOwner"),
+                        q.col("telstracellAttributes|hasSpecialEvent"),
+                        q.col("telstracellAttributes|hassignificantSpecialEvent"),
+                        q.col("telstracellattributes|mobileswitchingCentre"),
+                        q.col("telstraCellAttributes|mobileServiceArea"),
+                        q.col("telstracellAttributes|quolLindex"),
+                        q.col("telstraCellAttributes|hasHighSeasonality"));
+
+//q.show();
+        Dataset sites = (q
+                .select(q.col("base_station_name").alias("name"),
+                        q.col("base_station_name").alias("Srefld"),
+                        q.col("state").alias("stateProv"),
+                        q.col("nm_address_id").alias("siteId").cast(IntegerType$.MODULE$)//  = # cleanly converts to an integer
+                )
+                .distinct()
+                .withColumn("$type", functions.lit("oci/site"))
+                .withColumn("status", functions.lit("Live"))
+                .withColumn("type", functions.lit("OTHER"))
+                .withColumn("$action", functions.lit("createOrUpdate"))
+                .select("$type", "name", "$retId", "$action", "status", "type", "stateProv", "siteId")    //                      #this is just to re-ord
+        );
+//sites.show();
+        sites.write().mode("overwrite").json(Constants.bucketurl + bucketOutputPath + "site");
+
+        Dataset bsc = (q.where(q.col("technology").like("GSM%").and(q.col("bsc_rnc_node").isNotNull()))
+                .select(q.col("bsc_rnc_node").alias("name"))
+                .distinct()
+                .withColumn("status", functions.lit("UNKNOWN"))
+                .withColumn("$type", functions.lit("ocw/bsc"))
+                .withColumn("$action", functions.lit("createrOrUpdate"))
+                .withColumn("$refId", functions.col("name"))
+                .select("$type", "$action", "$refld", "name", "status")           //  #  datasync requires the attributes to be first
+        );
+
+        bsc.write().mode("overwrite").json(Constants.bucketurl + bucketOutputPath + "bsc");
+//                #bsc.show()
+
+//        q.show();
+        Dataset rnc = (q.where(functions.not(q.col("technology").like("GSM%")).and(q.col("bsc_rnc_node").isNotNull())))
+                .select(q.col("bsc_rnc_node").alias("name"))
+                .distinct()
+                .withColumn("status", functions.lit("UNKNOWN"))
+                .withColumn("$type", functions.lit("ocw/rnc"))
+                .withColumn("$action", functions.lit("createOrUpdate"))
+                .withColumn("$refId", functions.col("name"))
+                .select("$type", "$action", "$refId", "name", "status");
+        rnc.write().mode("overwrite").json(Constants.bucketurl + bucketOutputPath + "bsc");
+        rnc.show();
 
     }
 }
